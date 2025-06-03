600ad79c8cf55e8335d56799b84fed55ae003b03
Brine <brian@qualityshepherd.com>
2025-06-03 06:51:18 -0600

    SPLOG2

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+  title: 'Bulls Back Office',
+  description: 'Tracksuit executives one couch away from a champion. No access. No sources. No filters.',
+  url: 'https://bullsbackoffice.com',
+  pathToIndex: './index.json',
+  pathToPostsFolder: './posts',
+  pathToRssFolder: './assets/rss',
+  maxPosts: 10
+}
+
+export default config
