ef810fc4c40cd1d7ce93b91d5e5ddacdf551e158
Brine <brian@qualityshepherd.com>
2026-01-24 11:41:13 -0700

    rss

--- src/templates.js
+++ src/templates.js
@@ -26,12 +26,13 @@ export const aboutPageTemplate = () => `
   <h2>Bulls Back Office</h2>
   <div class="center">
     Tracksuit executives one couch away from a champion. No access. No sources. No filters.<br><br>
-    <b>Bulls Back Office Podcast</b> is recorded live from the discord server. Quality hoops talk with regular Bulls fans.
+    <b>Bulls Back Office Podcast</b> is recorded live from the discord server. Quality hoops talk with irregular Bulls fans.
     <div class="social">
       <a href="mailto:&#105;&#110;&#116;&#101;&#114;&#110;&#64;&#98;&#117;&#108;&#108;&#115;&#98;&#97;&#99;&#107;&#111;&#102;&#102;&#105;&#99;&#101;&#46;&#99;&#111;&#109;" title="email"><img src="assets/images/social/email.png" alt="email"></a>
       <a href="https://podcasts.apple.com/au/podcast/play-worlds-podcast/id1722152993" title="Bulls Back Office Podcast"><img src="assets/images/social/podcast.png" alt="Podcast"></a>
-      <a href="https://www.youtube.com/@bullsbackoffice" title="youtube"><img src="assets/images/social/youtube.png" alt="YouTube"></a>
-      <a href="/assets/rss/blog.xml"><img src="assets/images/social/rss.png" title="blog rss" alt="RSS"></a>
+      <a href="https://open.spotify.com/show/31eP1X3iWGOwSo4hnXsQ8S" title="Bulls Back Office Podcast"><img src="assets/images/social/spotify.png" alt="Podcast"></a>
+      <a href="https://www.youtube.com/playlist?list=PLuYK1nk87vUxxP1erLzuOoh2LmqQaA-lQ" title="youtube"><img src="assets/images/social/youtube.png" alt="YouTube"></a>
+      <a href="/assets/rss/pod.xml"><img src="assets/images/social/rss.png" title="pod rss" alt="RSS"></a>
     </div>
     <div class="center"><img src="assets/images/bbo_square_300.png"></div>
   </div>
